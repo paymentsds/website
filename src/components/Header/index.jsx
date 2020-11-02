53e02dc932e5741fa221a8971f7ad76f992363d1
Anisio Mandlate <anisiomandlate@gmail.com>
2020-11-02 12:28:29 +0200

    feat: Add elements on the navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,23 +3,28 @@ import PropTypes from "prop-types"
 import React from "react"
 
 import styles from "./styles.module.css"
+import logo from "../../images/payments.png"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div className={styles.menu}>
-      <h1>
-        <Link to="/" className={styles.links}>
-          {siteTitle}
-        </Link>
-      </h1>
-      <h4>
-        <Link to="/docs" className={styles.links}>
-          Documentação
-        </Link>
-      </h4>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+  return (
+    <header>
+      <div className={styles.menu}>
+        <img src={logo} alt={siteTitle} />
+        <nav className={styles.nav}>
+          <h4>Sobre nós</h4>
+          <h4>
+            <Link to="/docs" className={styles.links}>
+              Documentação
+            </Link>
+          </h4>
+          <h4>Fórum</h4>
+          <h4>Team</h4>
+          <h4>Contacto</h4>
+        </nav>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
