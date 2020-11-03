959bfa3367453a3cb83a8d7d6a4c60c866925e06
Adilson Mandlate <adilsonmandlate@gmail.com>
2020-11-03 10:48:39 +0200

    feat: change font family

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`poppins\:300,600`],
+        display: "swap",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
