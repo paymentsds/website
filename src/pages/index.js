ae3388fad913392262040df895695209f46028f5
Anisio Mandlate <anisiomandlate@gmail.com>
2020-11-18 13:29:30 +0200

    style: change layout for details section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/Layout"
 import Home from "../components/Home"
 import SEO from "../components/SEO/seo"
 import Cards from "../components/Cards"
+import "./styles.css"
 
 const IndexPage = () => (
   <Layout>
