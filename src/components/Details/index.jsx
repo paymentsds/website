baa4a40eb6eac75837435662c1f5426d44b1c666
Anisio Mandlate <anisiomandlate@gmail.com>
2020-11-18 17:25:27 +0200

    feat: add Card component on Details and made tweaks to Cards component for the details section

--- src/components/Details/index.jsx
+++ src/components/Details/index.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import styles from "./styles.module.css"
+import Cards from "../Cards"
 
 const DetailsTexts = () => {
   return (
@@ -18,21 +19,35 @@ const DetailsTexts = () => {
 
       <div className={styles.detailsColumns}>
         <div className={styles.column}>
-          <h4 className={styles.columnTitle}>Title</h4>
-          <p className={styles.columnText}>Paragraph</p>
+          <h4 className={styles.columnTitle}>SDKs</h4>
+          <p className={styles.columnText}>
+            Comece rapidamente a integrar a tua aplicação. Use nossos SDKs PHP,
+            JS, Java, Python, Ruby,e mais.
+          </p>
           <Link to="/page-2" className={styles.columnLink}>
-            Link
+            Use os SDKs
           </Link>
         </div>
 
         <div className={styles.column}>
-          <h4 className={styles.columnTitle}>Title</h4>
-          <p className={styles.columnText}>Paragraph</p>
+          <h4 className={styles.columnTitle}>Blog</h4>
+          <p className={styles.columnText}>
+            Receba as últimas notícias, informações e dicas sobre a plataforma
+            paymentsds e sobre o sector das Fintech local.
+          </p>
           <Link to="/page-2" className={styles.columnLink}>
-            Link
+            Ver Blog
           </Link>
         </div>
       </div>
+
+      <Cards
+        title="Fórum"
+        description="Junte-se à nossa comunidade, partilha as tuas dúvidas e ajuda a developers como tu a integrarem a suas aplicações as plataformas de pagamento."
+        link="page-2"
+        linkText="Aceder aos fóruns"
+        isDetails
+      />
     </div>
   )
 }
