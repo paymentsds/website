4de3629834f530f784c7158e3389fd91384b0fb2
Anisio Mandlate <anisiomandlate@gmail.com>
2020-11-18 18:36:40 +0200

    feat: add content on Terminal component

--- src/components/Details/Terminal.jsx
+++ src/components/Details/Terminal.jsx
@@ -1,11 +1,37 @@
 import React from "react"
+import ReactMarkdown from "react-markdown"
 
 import styles from "./terminal.module.css"
 
 const Terminal = () => {
+  const markdown = `
+    import { Client } from "@paymentsds/mpesa"
+
+    const client = new Client({
+        apiKey: "<REPLACE>", 
+        publicKey: "<REPLACE>", 
+        serviceProviderCode: "<REPLACE>", 
+    })
+
+    const paymentData = {
+        from: "841234567", 
+        reference: "11114",
+        transation: "T12344CC", 
+        amount: "10", 
+    }
+
+    client
+        .receive(paymentData)
+        .then(r => {
+            console.log(r.data)
+        })
+        .catch(e => {
+            console.log(e)
+        })
+  `
   return (
     <div className={styles.main}>
-      <h1>Hello Terminal</h1>
+      <ReactMarkdown children={markdown} />
     </div>
   )
 }
