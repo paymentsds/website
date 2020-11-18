c2e15982a69913275374f94838087deec8049bf2
Anisio Mandlate <anisiomandlate@gmail.com>
2020-11-18 13:25:25 +0200

    feat: add the first layout for the details section

---
+++ src/components/Details/Terminal.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+import styles from "./terminal.module.css"
+
+const Terminal = () => {
+  return <div></div>
+}
+
+export default Terminal
---
+++ src/components/Details/index.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+import styles from "./styles.module.css"
+
+const DetailsTexts = () => {
+  return <div></div>
+}
+
+export default DetailsTexts
