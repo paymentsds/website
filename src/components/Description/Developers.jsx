88220f7309282277da3ad6a358f3c6d3ddc2691b
Anisio Mandlate <anisiomandlate@gmail.com>
2020-12-10 08:58:03 +0200

    feat: Finish Developer section

--- src/components/Description/Developers.jsx
+++ src/components/Description/Developers.jsx
@@ -1,8 +1,9 @@
 import React from "react"
-import {Link} from "gatsby"
+import { Link } from "gatsby"
 
 import styles from "./developers.module.css"
 import devImage from "../../images/developerImage.svg"
+import Button from "../Button"
 
 const Developers = () => {
   return (
@@ -15,9 +16,26 @@ const Developers = () => {
           <h2>Começando</h2>
         </div>
         <div className={styles.texts}>
-        <p>
-        Obtenha as chaves da API de integração.<br/> Paymentsds autentica todas as solicitações de API usando as chaves de API de integração.<br/> Você precisará das chaves de API para fazer solicitações à API com êxito.<br/> Você pode criar integrações e chaves por meio do Painel, <span><Link to="/page-2">conforme descrito aqui</Link></span>
-        </p>
+          <p>
+            Obtenha as chaves da API de integração.
+            <br /> Paymentsds autentica todas as solicitações de API usando as
+            chaves de API de integração.
+            <br /> Você precisará das chaves de API para fazer solicitações à
+            API com êxito.
+            <br /> Você pode criar integrações e chaves por meio do Painel,{" "}
+            <span>
+              <Link to="/page-2">conforme descrito aqui</Link>
+            </span>
+          </p>
+        </div>
+        <div className={styles.buttonsContainer}>
+          <Link to="/docs">
+            <Button text="Ver o Repositório" />
+          </Link>
+
+          <Link to="https://github.com/paymentsds">
+            <Button text="Ver a Documentação" />
+          </Link>
         </div>
       </div>
       <div className={styles.image}>
