70a6c0f91cb3b31533a5cc5823767c8f0425a564
Anisio Mandlate <anisiomandlate@gmail.com>
2020-12-10 07:34:27 +0200

    feat: Add the first files for Description component

---
+++ src/components/Description/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import styles from './styles.module.css'
+
+const Description = () => {
+    return (
+        <div className={styles.main}>
+
+        </div>
+    )
+}
+
+export default Description
