a5b45a6c4c434ec2428e5e0d958a2816c468ebe5
Anisio Mandlate <anisiomandlate@gmail.com>
2020-12-11 20:34:35 +0200

    change: Make a verification for footerData and change the links verification

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,21 +6,24 @@ import styles from "./styles.module.css"
 import footerData from "../../data/links.yaml"
 
 const Footer = () => {
+  if (!footerData) {
+    return null
+  }
+
   return (
     <footer className={styles.footer}>
-      {/* Create a .yaml file to store all these links (and a possible description) */}
       <section className={styles.links}>
         <div>
           <h3 className={styles.title}>Documentação</h3>
           <ul>
             <li>
-              <a href={footerData?.links?.docs?.starting}>Começando</a>
+              <a href={footerData.links.docs.starting}>Começando</a>
             </li>
             <li>
-              <a href={footerData?.links?.docs?.api}>Referência API</a>
+              <a href={footerData.links.docs.api}>Referência API</a>
             </li>
             <li>
-              <a href={footerData?.links?.docs?.faq}>Perguntas frequentes</a>
+              <a href={footerData.links.docs.faq}>Perguntas frequentes</a>
             </li>
           </ul>
         </div>
@@ -28,19 +31,19 @@ const Footer = () => {
           <h3 className={styles.title}>Bibliotecas</h3>
           <ul>
             <li>
-              <a href={footerData?.links?.libraries?.java}>Java</a>
+              <a href={footerData.links.libraries.java}>Java</a>
             </li>
             <li>
-              <a href={footerData?.links?.libraries?.javascript}>Javascript</a>
+              <a href={footerData.links.libraries.javascript}>Javascript</a>
             </li>
             <li>
-              <a href={footerData?.links?.libraries?.php}>PHP</a>
+              <a href={footerData.links.libraries.php}>PHP</a>
             </li>
             <li>
-              <a href={footerData?.links?.libraries?.python}>Python</a>
+              <a href={footerData.links.libraries.python}>Python</a>
             </li>
             <li>
-              <a href={footerData?.links?.libraries?.ruby}>Ruby</a>
+              <a href={footerData.links.libraries.ruby}>Ruby</a>
             </li>
           </ul>
         </div>
@@ -48,13 +51,13 @@ const Footer = () => {
           <h3 className={styles.title}>Plugins</h3>
           <ul>
             <li>
-              <a href={footerData?.links?.plugins?.ecommerce}>E-Comerce</a>
+              <a href={footerData.links.plugins.ecommerce}>E-Comerce</a>
             </li>
             <li>
-              <a href={footerData?.links?.plugins?.social}>Social</a>
+              <a href={footerData.links.plugins.social}>Social</a>
             </li>
             <li>
-              <a href={footerData?.links?.plugins?.cloud}>Cloud</a>
+              <a href={footerData.links.plugins.cloud}>Cloud</a>
             </li>
           </ul>
         </div>
@@ -62,21 +65,21 @@ const Footer = () => {
           <h3 className={styles.title}>Comunidade</h3>
           <ul>
             <li>
-              <a href={footerData?.links?.community?.foruns}>
+              <a href={footerData.links.community.foruns}>
                 Fóruns de discussão
               </a>
             </li>
             <li>
-              <a href={footerData?.links?.community?.blog}>Blog</a>
+              <a href={footerData.links.community.blog}>Blog</a>
             </li>
             <li>
-              <a href={footerData?.links?.community?.github}>Github</a>
+              <a href={footerData.links.community.github}>Github</a>
             </li>
             <li>
-              <a href={footerData?.links?.community?.linkedin}>Linkedin</a>
+              <a href={footerData.links.community.linkedin}>Linkedin</a>
             </li>
             <li>
-              <a href={footerData?.links?.community?.youtube}>Youtube</a>
+              <a href={footerData.links.community.youtube}>Youtube</a>
             </li>
           </ul>
         </div>
