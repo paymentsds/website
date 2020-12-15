849af21647b40425aba6bf2236357cd8c2e5406e
Anisio Mandlate <anisiomandlate@gmail.com>
2020-12-15 10:54:18 +0200

    feat: Create an array for the links on the .yaml file and change the listing of the links on the Footer Component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -16,71 +16,49 @@ const Footer = () => {
         <div>
           <h3 className={styles.title}>Documentação</h3>
           <ul>
-            <li>
-              <a href={footerData.links.docs.starting}>Começando</a>
-            </li>
-            <li>
-              <a href={footerData.links.docs.api}>Referência API</a>
-            </li>
-            <li>
-              <a href={footerData.links.docs.faq}>Perguntas frequentes</a>
-            </li>
+            {footerData.links.docs.map(link => {
+              return (
+                <li>
+                  <a href={link.url}>{link.name}</a>
+                </li>
+              )
+            })}
           </ul>
         </div>
         <div>
           <h3 className={styles.title}>Bibliotecas</h3>
           <ul>
-            <li>
-              <a href={footerData.links.libraries.java}>Java</a>
-            </li>
-            <li>
-              <a href={footerData.links.libraries.javascript}>Javascript</a>
-            </li>
-            <li>
-              <a href={footerData.links.libraries.php}>PHP</a>
-            </li>
-            <li>
-              <a href={footerData.links.libraries.python}>Python</a>
-            </li>
-            <li>
-              <a href={footerData.links.libraries.ruby}>Ruby</a>
-            </li>
+            {footerData.links.libraries.map(link => {
+              return (
+                <li>
+                  <a href={link.url}>{link.name}</a>
+                </li>
+              )
+            })}
           </ul>
         </div>
         <div>
           <h3 className={styles.title}>Plugins</h3>
           <ul>
-            <li>
-              <a href={footerData.links.plugins.ecommerce}>E-Comerce</a>
-            </li>
-            <li>
-              <a href={footerData.links.plugins.social}>Social</a>
-            </li>
-            <li>
-              <a href={footerData.links.plugins.cloud}>Cloud</a>
-            </li>
+            {footerData.links.plugins.map(link => {
+              return (
+                <li>
+                  <a href={link.url}>{link.name}</a>
+                </li>
+              )
+            })}
           </ul>
         </div>
         <div>
           <h3 className={styles.title}>Comunidade</h3>
           <ul>
-            <li>
-              <a href={footerData.links.community.foruns}>
-                Fóruns de discussão
-              </a>
-            </li>
-            <li>
-              <a href={footerData.links.community.blog}>Blog</a>
-            </li>
-            <li>
-              <a href={footerData.links.community.github}>Github</a>
-            </li>
-            <li>
-              <a href={footerData.links.community.linkedin}>Linkedin</a>
-            </li>
-            <li>
-              <a href={footerData.links.community.youtube}>Youtube</a>
-            </li>
+            {footerData.links.community.map(link => {
+              return (
+                <li>
+                  <a href={link.url}>{link.name}</a>
+                </li>
+              )
+            })}
           </ul>
         </div>
       </section>
